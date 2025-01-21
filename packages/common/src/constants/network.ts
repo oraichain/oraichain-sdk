cb536ef347b591fdc4961acd14e4aa77584f065d
Hau Nguyen Van <nguyenvanhauit1997@gmail.com>
2025-01-21 14:08:24 +0700

    fix type network

--- packages/common/src/constants/network.ts
+++ packages/common/src/constants/network.ts
@@ -4,8 +4,10 @@ export const NETWORK_TYPES = {
   BSC: "bsc",
   TRON: "tron",
   BITCOIN: "bitcoin",
+  TON: "ton",
+  SVM: "svm"
 } as const;
 
 export type NetworkType = (typeof NETWORK_TYPES)[keyof typeof NETWORK_TYPES];
 
-export type CoinType = 0 | 118 | 60 | 195; // 0 for bitcoin, 118 for cosmos, 60 for evm, 195 for tron??
+export type CoinType = 0 | 118 | 60 | 195 | 501 | 607; // 0 for bitcoin, 118 for cosmos, 60 for evm, 195 for tron, 501 for solana, 607 for ton??
